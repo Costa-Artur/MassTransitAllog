73e1cad686a478d18d91f9c4c0e33ff516392603
Artur <souza.artur@edu.univali.br>
2023-07-31 15:15:26 -0300

    primeiro commit

---
+++ MassTransitConsumer.ConsoleApp/Shared/INotificationCreated.cs
@@ -0,0 +1,10 @@
+namespace MassTransitAllog.Shared;
+
+public interface INotificationCreated 
+{
+    public DateTime Date {get;set;}
+
+    public string Message {get;set;} 
+
+    public string Author {get;set;}
+}
